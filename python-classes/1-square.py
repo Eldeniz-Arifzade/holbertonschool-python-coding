9d26dd2db9d604f2d373ae253d45d734e84ba7e7
Eldeniz_Arifzade <eldeniz.2006.arifzade@gmail.com>
2025-12-09 13:28:10 +0000

    same class with positive integer size attr

---
+++ python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This module will intrduce a square class with size attribute
+
+Attribute should have an positive integer private size attr"""
+
+
+class Square():
+    """This class will have a private + integer size attr"""
+    def __init__(self, size=0):
+        """Initialize instance with + integer size"""
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        elif not isinstance(size, int):
+            raise TypeError('size must be an integer')
