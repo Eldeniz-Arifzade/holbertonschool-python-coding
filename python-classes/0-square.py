7340c5c44d2bb395103e8e04dd6c269bc2ff308e
Eldeniz_Arifzade <eldeniz.2006.arifzade@gmail.com>
2025-12-09 13:19:12 +0000

    add new line at the end

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 '''This model will define a square class,
  which will have a private size attribute'''
- 
+
 class Square():
     '''This class will define private instance attribute'''
     def __init__(self, size):
