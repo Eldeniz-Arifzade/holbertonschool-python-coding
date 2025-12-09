528863984c0f398af2c8843f8bfed4c7c3fdbeb6
Eldeniz_Arifzade <eldeniz.2006.arifzade@gmail.com>
2025-12-09 13:28:54 +0000

    init size

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -12,3 +12,4 @@ class Square():
             raise ValueError('size must be >= 0')
         elif not isinstance(size, int):
             raise TypeError('size must be an integer')
+        self.__size = size
