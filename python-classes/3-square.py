09d3bca50dc3ea02104b16d66bc7b6235fe23dc6
Eldeniz_Arifzade <eldeniz.2006.arifzade@gmail.com>
2025-12-09 14:10:59 +0000

    correct the mistakes

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,28 +1,28 @@
 #!/usr/bin/python3
-"""This module will intrduce a square class with size attribute
+"""This module will introduce a square class with size attribute
 
 Attribute should have an positive integer private size attr"""
 
 
-class Square():
+class Square:
     """This class will have a private + integer size attr
 
     It will validate the size and calculate the area"""
     def __init__(self, size=0):
         """Initialize instance with size"""
-        self.__size = size
+        self.size = size
 
     @property
-    def size(self, size):
+    def size(self):
         """Retrieve the size"""
         return self.__size
     
     @size.setter
-    def size(self, size, new_size):
+    def size(self, new_size):
         """Assign new_size to size"""
-        if not isinstance(size, int):
+        if not isinstance(new_size, int):
             raise TypeError('size must be an integer')
-        elif size < 0:
+        elif new_size < 0:
             raise ValueError('size must be >= 0')
         self.__size = new_size
 
