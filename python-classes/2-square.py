4c8f192ab91ddf20b9e592df6a4e8bee2c94aae0
Eldeniz_Arifzade <eldeniz.2006.arifzade@gmail.com>
2025-12-09 13:43:20 +0000

    correct pep8 errors

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -6,7 +6,7 @@ Attribute should have an positive integer private size attr"""
 
 class Square():
     """This class will have a private + integer size attr
-    
+
     It will validate the size and calculate the area"""
     def __init__(self, size=0):
         """Initialize instance with + integer size"""
@@ -15,6 +15,7 @@ class Square():
         elif size < 0:
             raise ValueError('size must be >= 0')
         self.__size = size
+
     def area(self):
         """Calculate the area of the square"""
         return self.__size ** 2
