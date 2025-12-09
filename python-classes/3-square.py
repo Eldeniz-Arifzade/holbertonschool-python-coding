4a488140d66cff824bbc4da0863b661df5444e09
Eldeniz_Arifzade <eldeniz.2006.arifzade@gmail.com>
2025-12-09 14:07:31 +0000

    access and update

---
+++ python-classes/3-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""This module will intrduce a square class with size attribute
+
+Attribute should have an positive integer private size attr"""
+
+
+class Square():
+    """This class will have a private + integer size attr
+
+    It will validate the size and calculate the area"""
+    def __init__(self, size=0):
+        """Initialize instance with size"""
+        self.size = size
+
+    @property
+    def size(self, size):
+        """Retrieve the size"""
+        return self.__size
+    
+    @size.setter
+    def size(self, size, new_size):
+        """Assign new_size to size"""
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = new_size
+
+    def area(self):
+        """Calculate the area of the square"""
+        return self.__size ** 2
