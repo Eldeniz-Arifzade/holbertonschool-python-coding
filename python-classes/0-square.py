4f8e58c0649ffc315ab80633b55b3f75c64154c1
Eldeniz_Arifzade <eldeniz.2006.arifzade@gmail.com>
2025-12-09 13:16:20 +0000

    gitpusher

---
+++ python-classes/0-square.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+class Square():
+    '''This class will define private instance attribute'''
+    def __init__(self, size):
+        '''Initialize class with size'''
+        self.__size = size
